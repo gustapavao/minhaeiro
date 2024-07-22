8a620beccdd7ab7e0d24babb691f42bc84e8ac5c
Gustavo Pavão <luisgustavopavao@hotmail.com>
2024-07-22 13:33:02 -0300

    Test Hello

---
+++ src/main/java/com/pavao/minhaeiro/MinhaeiroApplication.java
@@ -0,0 +1,13 @@
+package com.pavao.minhaeiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaeiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaeiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pavao/minhaeiro/MinhaeiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavao.minhaeiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhaeiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
